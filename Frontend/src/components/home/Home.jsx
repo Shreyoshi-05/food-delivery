88179959a9d2e4470a1e37372f97d0f5e14beecd
Shreyaoshi Das <sdas200010@gmail.com>
2025-10-26 00:09:33 +0530

    added line

--- Frontend/src/components/home/Home.jsx
+++ Frontend/src/components/home/Home.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { UseAppContext } from '../context/AppContext'
-import Login from '../popuplogin/Login';
+import React, { useEffect, useState } from 'react'
 import './home.css'
 import Hero from './innerHome/Hero';
 import Sections from './innerHome/Sections'
 import CardSec from './innerHome/CardSec';
 
 const Home = () => {
+  const [pageHight , setPageHight] = useState(0);
+
+  useEffect(()=>{
+    function handelScroll (){
+
+      let scrollhigth = Math.round(document.documentElement.scrollHeight);
+      let scrolled = Math.round(window.scrollY);
+      let shown = Math.round(window.innerHeight);
+      let total = scrolled+shown;
+
+      let per = Math.round(((total-shown) * 100)/(scrollhigth - shown));
+      setPageHight(per);
+    }
+
+    window.addEventListener("scroll",handelScroll)
+
+    return () => window.removeEventListener("scroll",handelScroll)
+  },[]);
 
   return (
+    <div className="page">
+      <div className="line">
+        <div style={{"width":`${pageHight}%`}} className="inner_line"></div>
+      </div>
     <div className="home">
       <Hero />
       <Sections />
       <CardSec />
+    </div>
     </div>
   )
 }
